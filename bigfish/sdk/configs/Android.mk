fea3f35aa63568cc35bcde32920bedcb6da27c82
wudilsr <wudilsr@gmail.com>
2022-12-28 11:07:55 +0800

    modify pq_param.bin position

--- bigfish/sdk/configs/Android.mk
+++ bigfish/sdk/configs/Android.mk
@@ -3,15 +3,4 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 include $(SDK_DIR)/Android.def
 
-PQIMG_NAME := pq_param.bin
-PQIMG_EMMC_DST_FILE := $(PRODUCT_OUT)/Emmc/pq_param.bin
-PQIMG_SRC_FILE := $(LOCAL_PATH)/$(CFG_HI_CHIP_TYPE)/prebuilts/$(PQIMG_NAME)
-
-$(PQIMG_EMMC_DST_FILE): $(PQIMG_SRC_FILE) | $(ACP)
-	@echo "install Hisilicon Emmc PQ images ..."
-	@mkdir -p $(dir $@)
-	$(ACP) -fvp "$(PQIMG_SRC_FILE)" "$(PQIMG_EMMC_DST_FILE)"
-
-pq_param: $(PQIMG_EMMC_DST_FILE)
-
 CUSTOM_MODULES += pq_param
